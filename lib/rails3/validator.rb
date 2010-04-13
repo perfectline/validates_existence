6f619efb021042ba21a7b752105e7b1d5c0dd52c
Tanel Suurhans <tanel.suurhans@perfectline.ee>
2010-04-14 02:51:55 +0300

    - rails 3 support

--- lib/rails3/validator.rb
+++ lib/rails3/validator.rb
@@ -1,43 +1,45 @@
-module ActiveModel
-  module Validations
+module Perfectline
+  module ValidatesExistence
 
-    class ExistenceValidator < EachValidator
+    module InstanceMethods
+      class ExistenceValidator < ActiveModel::EachValidator
 
-      def initialize(options)
-        # set the default message if its unspecified
-        options[:message] ||= :existence
-        super(options)
-      end
-
-      def validate_each(record, attribute, value)
-        normalized = attribute.to_s.sub(/_id$/, "").to_sym
-        association = record.class.reflect_on_association(normalized)
-
-        if association.nil? or !association.belongs_to?
-          raise ArgumentError, "Cannot validate existence on #{normalized}, not a :belongs_to association"
+        def initialize(options)
+          # set the default message if its unspecified
+          options[:message] ||= :existence
+          super(options)
         end
 
-        target_class = nil
+        def validate_each(record, attribute, value)
+          normalized = attribute.to_s.sub(/_id$/, "").to_sym
+          association = record.class.reflect_on_association(normalized)
 
-        # dealing with polymorphic belongs_to
-        if association.options.has_key?(:foreign_type)
-          foreign_type = record.__send__(association.options.fetch(:foreign_type))
-          target_class = foreign_type.constantize unless foreign_type.nil?
-        else
-          target_class = association.klass
-        end
+          if association.nil? or !association.belongs_to?
+            raise ArgumentError, "Cannot validate existence on #{normalized}, not a :belongs_to association"
+          end
 
-        if target_class.nil? or !target_class.exists?(value)
-          record.errors.add(attribute, options[:message], :default => "does not exist")
+          target_class = nil
 
-          # add the error on both :relation and :relation_id
-          if options[:both]
-            normalized = attribute.to_s.ends_with?("_id") ? normalized : "#{attribute}_id"
-            record.errors.add(normalized, options[:message], :default => "does not exist")
+          # dealing with polymorphic belongs_to
+          if association.options.has_key?(:foreign_type)
+            foreign_type = record.__send__(association.options.fetch(:foreign_type))
+            target_class = foreign_type.constantize unless foreign_type.nil?
+          else
+            target_class = association.klass
+          end
+
+          if target_class.nil? or !target_class.exists?(value)
+            record.errors.add(attribute, options[:message], :default => "does not exist")
+
+            # add the error on both :relation and :relation_id
+            if options[:both]
+              normalized = attribute.to_s.ends_with?("_id") ? normalized : "#{attribute}_id"
+              record.errors.add(normalized, options[:message], :default => "does not exist")
+            end
           end
         end
-      end
 
+      end
     end
 
     module ClassMethods
--- lib/validates_existence.rb
+++ lib/validates_existence.rb
@@ -1,6 +1,10 @@
-if Rails::MAJOR >= 3
+if Rails::VERSION::MAJOR >= 3
   require "rails3/validator"
+
+  ActiveModel::Validations.__send__(:include, Perfectline::ValidatesExistence::InstanceMethods)
+  ActiveModel::Validations.__send__(:extend,  Perfectline::ValidatesExistence::ClassMethods)
 else
   require "rails2/validator"
-  ActiveRecord::Base.extend(Perfectline::ValidatesExistence)
+  
+  ActiveRecord::Base.__send__(:extend, Perfectline::ValidatesExistence)
 end
